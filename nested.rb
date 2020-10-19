b74d68479ba4d6336d29099ea69ceab4712e3da3
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-19 17:20:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -36,8 +35,9 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash =
